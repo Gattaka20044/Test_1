737110715996fd004691e045d82bf41e83cb097d
Gattaka20044 <nexti20044@mail.ru>
2022-07-04 00:27:54 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/test1/SplashScreen.kt
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.test1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
